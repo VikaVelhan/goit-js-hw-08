bc3db39de0a69390473a3a1bb1f112ace8743f3c
VikaVelhan <velganvika@gmail.com>
2022-11-13 00:54:20 +0200

    task-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,20 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryListEl = document.querySelector('.gallery');
+const markup = galleryItems
+  .map(img => {
+    return `<a class="gallery__item" href="${img.original}">
+  <img class="gallery__image" src="${img.preview}" alt="${img.description}" />
+</a>`;
+  })
+  .join('');
+galleryListEl.innerHTML = markup;
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
